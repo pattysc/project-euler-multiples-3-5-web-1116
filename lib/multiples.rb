ceb2b14992b667a183c7fa4ae62ddc001e291089
pattysc <pattys@users.noreply.github.com>
2017-01-04 09:55:31 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,34 @@
 # Enter your procedural solution here!
+def sum_multiples(n)
+  counter = 3
+  final = 0
+
+  while counter < n
+  	if counter%3 == 0
+  		final += counter
+  	elsif counter%5 == 0
+  		final += counter
+  	end
+
+    counter += 1
+  end
+
+  return final
+end
+
+def collect_multiples(n)
+  counter = 3
+  arr = []
+
+  while counter < n
+    if counter%3 == 0
+      arr << counter
+    elsif counter%5 == 0
+      arr << counter
+    end
+
+    counter += 1
+  end
+
+  return arr
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,43 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_accessor :n
+
+  def initialize(n)
+    @n = n
+  end
+
+  def collect_multiples
+    counter = 3
+    arr = []
+
+    while counter < n
+      if counter%3 == 0
+      	arr << counter
+      elsif counter%5 == 0
+      	arr << counter
+      end
+
+      counter += 1
+    end
+
+    return arr
+  end
+
+  def sum_multiples
+    counter = 3
+    final = 0
+
+    while counter < n
+      if counter%3 == 0
+      	final += counter
+      elsif counter%5 == 0
+      	final += counter
+      end
+
+      counter += 1
+    end
+
+    return final
+  end
+end
